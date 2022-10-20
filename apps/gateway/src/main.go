6f292b1011f160118726281666693e0f2a8b680b
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-10-21 00:22:00 +0700

    add config

--- apps/gateway/src/main.go
+++ apps/gateway/src/main.go
@@ -4,37 +4,51 @@ import (
 	"context"
 	"flag"
 	"log"
+	"os"
+	"strings"
 	"time"
 
+	"github.com/chula-overflow/chula-overflow-backend/apps/gateway/src/config"
 	"github.com/chula-overflow/chula-overflow-backend/apps/gateway/src/proto"
 	"github.com/gofiber/fiber/v2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var (
-	addr = flag.String("addr", "localhost:3001", "the address to connect to")
-	name = flag.String("name", "world", "Name to greet")
-)
-
 func main() {
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	port := strings.Split(conf.Gateway.Addr, ":")[1]
+
+	app := getServer(conf)
+
+	app.Listen(":" + port)
+}
+
+func getServer(conf *config.Config) *fiber.App {
 	app := fiber.New()
 
 	flag.Parse()
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(conf.Auth.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
+		os.Exit(2)
 	}
-	defer conn.Close()
+
 	authClient := proto.NewAuthClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	app.Get("/", func(c *fiber.Ctx) error {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
 		result, err := authClient.Login(ctx, &proto.AuthRequest{
 			Name: "Hello",
 		})
+
 		if err != nil {
 			log.Fatal(err)
 			return err
@@ -43,5 +57,5 @@ func main() {
 		return c.SendString(result.Token)
 	})
 
-	app.Listen(":3000")
+	return app
 }
