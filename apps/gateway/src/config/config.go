9db47749d9650e474e694d5b88cffc7fedd08775
imsozrious <pannawichtata@gmail.com>
2022-10-21 08:56:48 +0700

    fix config

---
+++ apps/gateway/src/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+// this code is generated in https://zhwt.github.io/yaml-to-go/
+type Config struct {
+	Gateway struct {
+		Addr string `yaml:"addr"`
+	} `yaml:"gateway"`
+	Auth struct {
+		Addr string `yaml:"addr"`
+	} `yaml:"auth"`
+}
+
+func LoadConfig() (*Config, error) {
+	var conf Config
+
+	yamlFile, err := ioutil.ReadFile("../config/config.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err #%v ", err)
+		return nil, err
+	}
+	err = yaml.Unmarshal(yamlFile, &conf)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+		return nil, err
+	}
+
+	return &conf, nil
+}
