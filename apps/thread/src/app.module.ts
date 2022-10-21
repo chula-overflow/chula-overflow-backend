ad06d42400a8c1a0591d7cfde44bc447f41bc2ac
Ebonian <tanadon.santisan@gmail.com>
2022-10-21 10:07:55 +0700

    chore: configure proto

--- apps/thread/src/app.module.ts
+++ apps/thread/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MicrController } from './micr/micr.controller';
+import { ThreadController } from './thread/thread.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, MicrController],
+  controllers: [AppController, MicrController, ThreadController],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/thread/src/main.ts
+++ apps/thread/src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.GRPC,
-    options: {
-      url: '0.0.0.0:3001',
-      protoPath: '../../proto/thread.proto',
-      package: 'micr',
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'micr',
+        protoPath: __dirname + '/../../proto/thread.proto',
+      },
     },
-  });
+  );
 
   // @ts-ignore
   app.listen(() => console.log('Microservice is listening'));
