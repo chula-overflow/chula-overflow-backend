fdd49e65070fbae3842f4900c6fdea54895179a5
Ebonian <tanadon.santisan@gmail.com>
2022-10-22 15:42:30 +0700

    init exam in course service

--- apps/course/src/app.module.ts
+++ apps/course/src/app.module.ts
@@ -7,6 +7,8 @@ import { MicrController } from './micr/micr.controller';
 import { Course, CourseSchema } from './models/course.schema';
 import { CourseController } from './course/course.controller';
 import { CourseService } from './course/course.service';
+import { ExamController } from './exam/exam.controller';
+import { ExamService } from './exam/exam.service';
 
 @Module({
   imports: [
@@ -14,7 +16,7 @@ import { CourseService } from './course/course.service';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]),
   ],
-  controllers: [AppController, MicrController, CourseController],
-  providers: [AppService, CourseService],
+  controllers: [AppController, MicrController, CourseController, ExamController],
+  providers: [AppService, CourseService, ExamService],
 })
 export class AppModule {}
