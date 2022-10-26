bafaf1eaf58f8454bb4bb3c5d5aab94b5a4ba304
Ebonian <tanadon.santisan@gmail.com>
2022-10-26 10:23:00 +0700

    comment thread service

--- apps/course/src/thread/thread.controller.ts
+++ apps/course/src/thread/thread.controller.ts
@@ -17,118 +17,118 @@ export class ThreadController {
     private readonly ExamService: ExamService,
   ) {}
 
-  @GrpcMethod()
-  async createThread(data: ThreadRequestCreateBody, metadata: any) {
-    const examId = await this.ExamService.findIdByCourseProperty(
-      data.year,
-      data.semester,
-      data.term,
-    );
+  // @GrpcMethod()
+  // async createThread(data: ThreadRequestCreateBody, metadata: any) {
+  //   const examId = await this.ExamService.findIdByCourseProperty(
+  //     data.year,
+  //     data.semester,
+  //     data.term,
+  //   );
 
-    const createThreadBody = {
-      exam_id: String(examId),
-      course_id: data.course_id,
-      upvoted: 0,
-      downvoted: 0,
-      problems: [
-        {
-          id: String(new Types.ObjectId()),
-          title: 'generated from nlp',
-          body: data.question,
-          uploaded_user: data.uploaded_user,
-          upvoted: 0,
-          downvoted: 0,
-        },
-      ],
-      answers: [
-        data.answer && {
-          id: String(new Types.ObjectId()),
-          body: data.answer,
-          upvoted: 0,
-          downvoted: 0,
-        },
-      ],
-    };
+  //   const createThreadBody = {
+  //     exam_id: String(examId),
+  //     course_id: data.course_id,
+  //     upvoted: 0,
+  //     downvoted: 0,
+  //     problems: [
+  //       {
+  //         id: String(new Types.ObjectId()),
+  //         title: 'generated from nlp',
+  //         body: data.question,
+  //         uploaded_user: data.uploaded_user,
+  //         upvoted: 0,
+  //         downvoted: 0,
+  //       },
+  //     ],
+  //     answers: [
+  //       data.answer && {
+  //         id: String(new Types.ObjectId()),
+  //         body: data.answer,
+  //         upvoted: 0,
+  //         downvoted: 0,
+  //       },
+  //     ],
+  //   };
 
-    const newThread = await this.ThreadService.createThread(createThreadBody);
+  //   const newThread = await this.ThreadService.createThread(createThreadBody);
 
-    const threadId = newThread._id;
+  //   const threadId = newThread._id;
 
-    // add thread_id to exam document
-    await this.ExamService.addThreadId(threadId, examId);
+  //   // add thread_id to exam document
+  //   await this.ExamService.addThreadId(threadId, examId);
 
-    return newThread;
-  }
+  //   return newThread;
+  // }
 
-  @GrpcMethod()
-  async getAllThreadsByExamProperty(
-    data: ExamPropertyRequestBody,
-    metadata: any,
-  ) {
-    const examId = await this.ExamService.findIdByCourseProperty(
-      data.year,
-      data.semester,
-      data.term,
-    );
+  // @GrpcMethod()
+  // async getAllThreadsByExamProperty(
+  //   data: ExamPropertyRequestBody,
+  //   metadata: any,
+  // ) {
+  //   const examId = await this.ExamService.findIdByCourseProperty(
+  //     data.year,
+  //     data.semester,
+  //     data.term,
+  //   );
 
-    const threads = await this.ThreadService.findByExamId(examId);
+  //   const threads = await this.ThreadService.findByExamId(examId);
 
-    return threads;
-  }
+  //   return threads;
+  // }
 
-  @GrpcMethod()
-  async getThreadById(data: ThreadIdRequestBody, metadata: any) {
-    const thread = await this.ThreadService.findOneById(data.thread_id);
+  // @GrpcMethod()
+  // async getThreadById(data: ThreadIdRequestBody, metadata: any) {
+  //   const thread = await this.ThreadService.findOneById(data.thread_id);
 
-    return thread;
-  }
+  //   return thread;
+  // }
 
-  @GrpcMethod()
-  async getThreadByTitle(data, metadata: any) {}
+  // @GrpcMethod()
+  // async getThreadByTitle(data, metadata: any) {}
 
-  @GrpcMethod()
-  async addAnswerById(data: ThreadRequestAddAnswerBody, metadata: any) {
-    const thread = await this.ThreadService.findOneById(data.thread_id);
+  // @GrpcMethod()
+  // async addAnswerById(data: ThreadRequestAddAnswerBody, metadata: any) {
+  //   const thread = await this.ThreadService.findOneById(data.thread_id);
 
-    const threadId = thread._id;
-    const answer = {
-      body: data.body,
-      upvoted: 0,
-      downvoted: 0,
-    };
+  //   const threadId = thread._id;
+  //   const answer = {
+  //     body: data.body,
+  //     upvoted: 0,
+  //     downvoted: 0,
+  //   };
 
-    const updatedThread = await this.ThreadService.updateThreadById(threadId, {
-      answers: [...thread.answers, answer],
-    });
+  //   const updatedThread = await this.ThreadService.updateThreadById(threadId, {
+  //     answers: [...thread.answers, answer],
+  //   });
 
-    return updatedThread;
-  }
+  //   return updatedThread;
+  // }
 
-  @GrpcMethod()
-  async upvoteThread(data: ThreadIdRequestBody, metadata: any) {
-    const thread = await this.ThreadService.findOneById(data.thread_id);
+  // @GrpcMethod()
+  // async upvoteThread(data: ThreadIdRequestBody, metadata: any) {
+  //   const thread = await this.ThreadService.findOneById(data.thread_id);
 
-    const threadId = thread._id;
-    const updatedUpvoted = thread.upvoted + 1;
+  //   const threadId = thread._id;
+  //   const updatedUpvoted = thread.upvoted + 1;
 
-    const updatedThread = await this.ThreadService.updateThreadById(threadId, {
-      upvoted: updatedUpvoted,
-    });
+  //   const updatedThread = await this.ThreadService.updateThreadById(threadId, {
+  //     upvoted: updatedUpvoted,
+  //   });
 
-    return updatedThread;
-  }
+  //   return updatedThread;
+  // }
 
-  @GrpcMethod()
-  async downvoteThread(data: ThreadIdRequestBody, metadata: any) {
-    const thread = await this.ThreadService.findOneById(data.thread_id);
+  // @GrpcMethod()
+  // async downvoteThread(data: ThreadIdRequestBody, metadata: any) {
+  //   const thread = await this.ThreadService.findOneById(data.thread_id);
 
-    const threadId = thread._id;
-    const updatedDownvoted = thread.downvoted - 1;
+  //   const threadId = thread._id;
+  //   const updatedDownvoted = thread.downvoted - 1;
 
-    const updatedThread = await this.ThreadService.updateThreadById(threadId, {
-      upvoted: updatedDownvoted,
-    });
+  //   const updatedThread = await this.ThreadService.updateThreadById(threadId, {
+  //     upvoted: updatedDownvoted,
+  //   });
 
-    return updatedThread;
-  }
+  //   return updatedThread;
+  // }
 }
--- apps/course/src/thread/thread.service.ts
+++ apps/course/src/thread/thread.service.ts
@@ -16,78 +16,78 @@ export class ThreadService {
     private ThreadModel: Model<ThreadDocument>,
   ) {}
 
-  async createThread(threadBody: ThreadCreateBody): Promise<ThreadBody> {
-    const newThread = new this.ThreadModel(threadBody);
-    return newThread.save();
-  }
+  // async createThread(threadBody: ThreadCreateBody): Promise<ThreadBody> {
+  //   const newThread = new this.ThreadModel(threadBody);
+  //   return newThread.save();
+  // }
 
-  async generateTitle() {}
+  // async generateTitle() {}
 
-  async findByExamId(examId: ObjectId) {
-    const threads = await this.ThreadModel.find({
-      exam_id: examId,
-    }).exec();
-    return threads;
-  }
+  // async findByExamId(examId: ObjectId) {
+  //   const threads = await this.ThreadModel.find({
+  //     exam_id: examId,
+  //   }).exec();
+  //   return threads;
+  // }
 
-  async findOneById(threadId: ObjectId) {
-    const thread = await this.ThreadModel.findById(threadId);
-    return thread;
-  }
+  // async findOneById(threadId: ObjectId) {
+  //   const thread = await this.ThreadModel.findById(threadId);
+  //   return thread;
+  // }
 
-  async updateThreadById(threadId: ObjectId, threadBody: ThreadUpdateBody) {
-    const updatedThread = this.ThreadModel.findByIdAndUpdate(
-      threadId,
-      { ...threadBody },
-      { new: true },
-    );
-    return updatedThread;
-  }
+  // async updateThreadById(threadId: ObjectId, threadBody: ThreadUpdateBody) {
+  //   const updatedThread = this.ThreadModel.findByIdAndUpdate(
+  //     threadId,
+  //     { ...threadBody },
+  //     { new: true },
+  //   );
+  //   return updatedThread;
+  // }
 
-  async updateProblemByProblemId(problemId: string) {}
+  // async updateProblemByProblemId(problemId: string) {}
 
-  async updateAnswerBodyByAnswerId(
-    answerId: string,
-    answerBody: ThreadAnswerUpdateBody,
-  ) {
-    const updatedThread = this.ThreadModel.updateOne(
-      { 'answers.id': answerId },
-      {
-        $set: {
-          'answer.$.body': answerBody.body,
-        },
-      },
-    );
-    return updatedThread;
-  }
+  // async updateAnswerBodyByAnswerId(
+  //   answerId: string,
+  //   answerBody: ThreadAnswerUpdateBody,
+  // ) {
+  //   const updatedThread = this.ThreadModel.updateOne(
+  //     { 'answers.id': answerId },
+  //     {
+  //       $set: {
+  //         'answer.$.body': answerBody.body,
+  //       },
+  //     },
+  //   );
+  //   return updatedThread;
+  // }
 
-  async updateAnswerUpvoteByAnswerId(
-    answerId: string,
-    answerBody: ThreadAnswerUpdateBody,
-  ) {
-    const updatedThread = this.ThreadModel.updateOne(
-      { 'answers.id': answerId },
-      {
-        $set: {
-          'answer.$.upvoted': answerBody.upvoted,
-        },
-      },
-    );
-    return updatedThread;
-  }
+  // async updateAnswerUpvoteByAnswerId(
+  //   answerId: string,
+  //   answerBody: ThreadAnswerUpdateBody,
+  // ) {
+  //   const updatedThread = this.ThreadModel.updateOne(
+  //     { 'answers.id': answerId },
+  //     {
+  //       $set: {
+  //         'answer.$.upvoted': answerBody.upvoted,
+  //       },
+  //     },
+  //   );
+  //   return updatedThread;
+  // }
 
-  async updateAnswerDownvoteByAnswerId(
-    answerId: string,
-    answerBody: ThreadAnswerUpdateBody,
-  ) {
-    const updatedThread = this.ThreadModel.updateOne(
-      { 'answers.id': answerId },
-      {
-        $set: {
-          'answer.$.downvoted': answerBody.downvoted,
-        },
-      },
-    );
-    return updatedThread;
-  }
+  // async updateAnswerDownvoteByAnswerId(
+  //   answerId: string,
+  //   answerBody: ThreadAnswerUpdateBody,
+  // ) {
+  //   const updatedThread = this.ThreadModel.updateOne(
+  //     { 'answers.id': answerId },
+  //     {
+  //       $set: {
+  //         'answer.$.downvoted': answerBody.downvoted,
+  //       },
+  //     },
+  //   );
+  //   return updatedThread;
+  // }
 }
